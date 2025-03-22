3daee4e0317d2841743a61034a830c60c0f2b4e8
caclm10 <lewinxander@gmail.com>
2025-03-22 13:54:57 +0700

    chore: setup github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
+import tailwindcss from "@tailwindcss/vite";
 import nodePath from "path";
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [tailwindcss()],
-  resolve: {
-    alias: {
-      "@": nodePath.resolve(__dirname, "src"),
+    base: "/simple-dialog/",
+    plugins: [tailwindcss()],
+    resolve: {
+        alias: {
+            "@": nodePath.resolve(__dirname, "src"),
+        },
     },
-  },
 });
